51e4dee8eb4efa528aeaf21976ee6656b6bd69ed
Henrique Marques <hmdarkfire.dev@gmail.com>
2023-12-21 18:27:31 -0300

    feat: improve architeture

--- index.js
+++
@@ -1,7 +0,0 @@
-// main index.js
-
-import { NativeModules } from 'react-native';
-
-const { CafFaceLiveness } = NativeModules;
-
-export default CafFaceLiveness;
--- ios/Pods/Target Support Files/Pods-CafFaceLiveness/Pods-CafFaceLiveness-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CafFaceLiveness : NSObject
-@end
-@implementation PodsDummy_Pods_CafFaceLiveness
-@end
