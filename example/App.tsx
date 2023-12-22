a1317bb256c24c9b1f41e3796830bf7bd401b57d
Henrique Marques <hmdarkfire.dev@gmail.com>
2023-12-22 11:13:05 -0300

    teste

--- example/App.tsx
+++ example/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { View, Button } from "react-native";
-
-import { useFaceLiveness } from "../src/hooks/useFaceLiveness";
+import { View, Button, Text } from "react-native";
 
 const App = () => {
   const mobileToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NTRjZmFlMWM5YTM0NTAwMDg4YzIwODUifQ.maH9fynasnaRR2Hm5PxQ1XzLxlVZiZSvpVDD9zVtfgs";
   const peopleId = "43485449806";
 
-  const { startFaceLiveness } = useFaceLiveness(mobileToken, peopleId);
+  // const { startFaceLiveness } = useFaceLiveness(mobileToken, peopleId);
 
   return (
     <View>
-      <Button title="Click me" onPress={startFaceLiveness} />
+      <Text>Teste</Text>
     </View>
   );
 };
