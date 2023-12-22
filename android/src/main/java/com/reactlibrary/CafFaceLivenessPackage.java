e46c7c87be9dc78ddc4b55d07eec39075f2811a7
Henrique Marques <hmdarkfire.dev@gmail.com>
2023-12-22 11:49:22 -0300

    fix: 3

--- android/src/main/java/com/reactlibrary/CafFaceLivenessPackage.java
+++ android/src/main/java/com/reactlibrary/CafFaceLivenessPackage.java
@@ -1,17 +1,17 @@
 package com.reactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import org.jetbrains.annotations.NotNull;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-public class CafFaceLivenessPackage implements ReactPackage {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CafPackage implements ReactPackage {
     @NotNull
     @Override
     public List<ViewManager> createViewManagers(@NotNull ReactApplicationContext reactContext) {
@@ -23,7 +23,7 @@ public class CafFaceLivenessPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(@NotNull ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
-        modules.add(new CafFaceLivenessModule(reactContext));
+        modules.add(new CafFaceLiveness(reactContext));
 
         return modules;
     }
