63f7eae84f4606c43ccc39e09da239040f49a98b
Henrique Marques <hmdarkfire.dev@gmail.com>
2023-12-22 12:49:12 -0300

    fix: fix

--- android/src/main/java/com/reactlibrary/CafFaceLiveness.java
+++ android/src/main/java/com/reactlibrary/CafFaceLiveness.java
@@ -1,32 +1,111 @@
 package com.reactlibrary;
 
-import android.content.Intent;
+import com.caf.facelivenessiproov.input.FaceLiveness;
+import com.caf.facelivenessiproov.input.VerifyLivenessListener;
+import com.caf.facelivenessiproov.output.FaceLivenessResult;
+import com.caf.facelivenessiproov.output.failure.NetworkReason;
+import com.caf.facelivenessiproov.output.failure.SDKFailure;
+import com.caf.facelivenessiproov.output.failure.ServerReason;
 
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import org.json.JSONException;
 
 import javax.annotation.Nonnull;
 
 public class CafFaceLiveness extends ReactContextBaseJavaModule {
-    private Intent intent;
+    private Context context;
+    private String customConfig;
+
+    CafFaceLiveness(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
+
     @Nonnull
     @Override
     public String getName() {
         return "CafFaceLiveness";
     }
 
-    CafFaceLiveness(ReactApplicationContext reactContext) {
-        super(reactContext);
-        intent = new Intent(getReactApplicationContext(), CafFaceLivenessActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    }
-
     @ReactMethod
     public void startFaceLiveness(String token, String personId, String config) {
-        intent.putExtra("token", token);
-        intent.putExtra("personId", personId);
-        intent.putExtra("config", config);
-        getReactApplicationContext().startActivity(intent);
+        customConfig = config;
+
+        FaceLivenessConfig formattedConfig = new FaceLivenessConfig(customConfig);
+
+        FaceLiveness faceLiveness = new FaceLiveness.Builder(token)
+                .setStage(formattedConfig.cafStage)
+                .setFilter(formattedConfig.filter)
+                .setEnableScreenshots(formattedConfig.setEnableScreenshots)
+                .setLoadingScreen(formattedConfig.setLoadingScreen)
+                .build();
+
+        faceLiveness.startSDK(this.context, personId, new VerifyLivenessListener() {
+            @Override
+            public void onSuccess(FaceLivenessResult faceLivenessResult) {
+                WritableMap writableMap = new WritableNativeMap();
+                writableMap.putString("data", faceLivenessResult.getSignedResponse());
+
+                getReactInstanceManager().getCurrentReactContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("FaceLiveness_Success", writableMap);
+                finish();
+            }
+
+            @Override
+            public void onError(FaceLivenessResult faceLivenessResult) {
+                String message = "Error: " + faceLivenessResult.getErrorMessage();
+                ;
+                String type = "Error";
+                WritableMap writableMap = new WritableNativeMap();
+                SDKFailure sdkFailure = faceLivenessResult.getSdkFailure();
+
+                if (sdkFailure instanceof NetworkReason) {
+                    message = ("FaceLivenessResult " + "onError: " + " Throwable: " + ((NetworkReason) faceLivenessResult.getSdkFailure()).getThrowable());
+                } else if (sdkFailure instanceof ServerReason) {
+                    message = ("FaceLivenessResult " + "onError: " + " Status Code: " + ((ServerReason) faceLivenessResult.getSdkFailure()).getCode());
+                    message = message + " Status Message: " + faceLivenessResult.getSdkFailure().getMessage();
+                }
+
+                writableMap.putString("message", message);
+                writableMap.putString("type", type);
+
+                getReactInstanceManager().getCurrentReactContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("FaceLiveness_Error", writableMap);
+                finish();
+            }
+
+            @Override
+            public void onCancel(FaceLivenessResult faceLivenessResult) {
+                WritableMap writableMap = new WritableNativeMap();
+                getReactInstanceManager().getCurrentReactContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("FaceLiveness_Cancel", writableMap);
+                finish();
+            }
+
+            @Override
+            public void onLoading() {
+                WritableMap writableMap = new WritableNativeMap();
+
+                getReactInstanceManager().getCurrentReactContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("FaceLiveness_Loading", writableMap);
+            }
+
+            @Override
+            public void onLoaded() {
+                WritableMap writableMap = new WritableNativeMap();
+
+                getReactInstanceManager().getCurrentReactContext()
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("FaceLiveness_Loaded", writableMap);
+            }
+        });
     }
 }
